485462222adaf43a77ea477f52f5d85590eefaed
suhwan2004 <suhwan2004@gmail.com>
2022-05-17 00:34:03 +0900

    do : mainPage basic structure and example created,  to-do : 1.weather api, shifting cody library page Method create 2.mainpage image slide create

--- app/src/main/java/com/example/cody_c/MainActivity.java
+++ app/src/main/java/com/example/cody_c/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.cody_c;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.cody_c.pagefragment.fragment_citysearch;
+import com.example.cody_c.pagefragment.fragment_clothstyle;
+import com.example.cody_c.pagefragment.fragment_main;
+import com.example.cody_c.pagefragment.fragment_main_codyimg;
+import com.example.cody_c.pagefragment.fragment_main_codyset;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment mainFragment, clothStyleFragment, citySearchFragment, mainCodyImgFragment, mainCodySetFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate (savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+        BottomNavigationView bottomView = findViewById(R.id.bottomNavigationView);
+        bottomView.setOnNavigationItemSelectedListener(listener);
+
+        createFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()){
+                case R.id.item_home:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
+                    return true;
+                case R.id.item_clothstyle:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, clothStyleFragment).commit();
+                    return true;
+                case R.id.item_citysearch:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, citySearchFragment).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void createFragment() {
+        mainFragment = new fragment_main();
+        clothStyleFragment = new fragment_clothstyle();
+        citySearchFragment = new fragment_citysearch();
+        mainCodyImgFragment = new fragment_main_codyimg();
+        mainCodySetFragment = new fragment_main_codyset();
     }
 }
---
+++ app/src/main/java/com/example/cody_c/pagefragment/fragment_main.java
@@ -0,0 +1,73 @@
+package com.example.cody_c.pagefragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cody_c.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragment_main#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragment_main extends Fragment {
+
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 2;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public fragment_main() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BlankFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static fragment_main newInstance(String param1, String param2) {
+        fragment_main fragment = new fragment_main();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
