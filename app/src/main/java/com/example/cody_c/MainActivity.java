0b9c0ff1721d6f855989f64d7d89e529a727d147
suhwan2004 <suhwan2004@gmail.com>
2022-05-17 15:01:14 +0900

    done : basic project structure 제작 do : MainActivity.java내에 deprecated된.코드 수정  todo : 각페이지 구현 및 추가기능 구현

--- app/src/main/java/com/example/cody_c/MainActivity.java
+++ app/src/main/java/com/example/cody_c/MainActivity.java
@@ -13,41 +13,39 @@ import com.example.cody_c.pagefragment.fragment_main;
 import com.example.cody_c.pagefragment.fragment_main_codyimg;
 import com.example.cody_c.pagefragment.fragment_main_codyset;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
     private Fragment mainFragment, clothStyleFragment, citySearchFragment, mainCodyImgFragment, mainCodySetFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("hello");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         FrameLayout frameLayout = findViewById(R.id.frameLayout);
         BottomNavigationView bottomView = findViewById(R.id.bottomNavigationView);
-        bottomView.setOnNavigationItemSelectedListener(listener);
+        bottomView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.item_home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
+                        return true;
+                    case R.id.item_clothstyle:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, clothStyleFragment).commit();
+                        return true;
+                    case R.id.item_citysearch:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, citySearchFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
 
         createFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
     }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()){
-                case R.id.item_home:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, mainFragment).commit();
-                    return true;
-                case R.id.item_clothstyle:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, clothStyleFragment).commit();
-                    return true;
-                case R.id.item_citysearch:
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, citySearchFragment).commit();
-                    return true;
-            }
-            return false;
-        }
-    };
 
     private void createFragment() {
         mainFragment = new fragment_main();
